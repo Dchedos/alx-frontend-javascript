fb74766ed5529a18e96faca267e2eae3835c5b32
Nchedo Okongwu <nchedookongwu@yahoo.com>
2023-06-26 23:21:29 +0000

    Write a function named createIteratorObject, that will take into argument a report Object created with the previous function createReportObject

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,32 +1,7 @@
 export default function createIteratorObject(report) {
-  let departmentIndex = 0;
-  let employeeIndex = 0;
-  const departments = Object.keys(report.allEmployees);
-
-  return {
-    next() {
-      if (departmentIndex >= departments.length) {
-        return {
-          done: true
-        };
-      }
-
-      const currentDepartment = departments[departmentIndex];
-      const employees = report.allEmployees[currentDepartment];
-      
-      if (employeeIndex >= employees.length) {
-        departmentIndex++;
-        employeeIndex = 0;
-        return this.next();
-      }
-
-      const currentEmployee = employees[employeeIndex];
-      employeeIndex++;
-
-      return {
-        value: currentEmployee,
-        done: false
-      };
-    }
-  };
+  const result = [];
+  for (const idx of Object.values(report.allEmployees)) {
+    result.push(...idx);
+  }
+  return result;
 }
