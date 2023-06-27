4e7eb7272d922b1d6a94c7cf0bdca9a28830e35c
Nchedo Okongwu <nchedookongwu@yahoo.com>
2023-06-27 22:39:59 +0000

    Write a function named iterateThroughObject. The function’s parameter reportWithIterator is the return value from createIteratorObject

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -3,3 +3,15 @@ export default function iterateThroughObject(reportWithIterator) {
     console.log('Placeholder');
   }
 }
+
+export default function iterateThroughObject(reportWithIterator) {
+  // Initialize an empty array to store the names
+  let names = [];
+  // Loop through the iterator object
+  for (let item of reportWithIterator) {
+    // Push each name to the array
+    names.push(item);
+  }
+  // Join the array elements with | and return the string
+  return names.join(" | ");
+}
