66d6e1491ad00c3a697dee02151bd331c03778f3
Nchedo Okongwu <nchedookongwu@yahoo.com>
2023-06-27 21:32:59 +0000

    Task 0. Return a Promise using this prototype function getResponseFromAPI()

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,14 @@
-export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous API call
+    setTimeout(() => {
+      const response = 'API response data';
+      // Resolve the Promise with the response
+      resolve(response);
+      
+      // Reject the Promise (uncomment the lines below to simulate an error)
+      // const error = 'An error occurred while fetching data from API';
+      // reject(error);
+    }, 2000); // Simulating a delay of 2 seconds
+  });
 }
