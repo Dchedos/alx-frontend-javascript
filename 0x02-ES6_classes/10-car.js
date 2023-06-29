46e8a2cf23fe50f6c75ca2cdada43c0a17cf4e7f
Nchedo Okongwu <nchedookongwu@yahoo.com>
2023-06-29 14:38:01 +0000

    Task 10. Implement a class named Car

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,15 +1,12 @@
-export default class Car {
+class Car {
   constructor(brand, motor, color) {
-    // Create objs
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  // Methods
-
   cloneCar() {
-    const NewObj = this.constructor[Symbol.species] || this.constructor;
-    const clone = new NewObj();
-    return clone;
+    const clonedCar = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+    return clonedCar;
   }
+}
